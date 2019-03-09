4a819595dfd1fe2adeefd532893565d0e8d13cb6
Yasas <yasass@lolctech.com>
2019-03-09 11:31:58 +0530

    Custom action bar added

--- CodefestCalculator/app/src/main/java/calculator/codefest_calculator/CalculatorActivity.java
+++ CodefestCalculator/app/src/main/java/calculator/codefest_calculator/CalculatorActivity.java
@@ -1,5 +1,6 @@
 package calculator.codefest_calculator;
 
+import android.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -12,8 +13,10 @@ public class CalculatorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setCustomView(R.layout.toolbar);
+
     }
 
 }
